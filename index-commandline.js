4ecb74771b8ef5eecadf975bd3a9fceeef14a5e8
minecrawler <me@marco-alka.de>
2017-06-23 15:47:54 +0200

    fix file loading

--- index-commandline.js
+++ index-commandline.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const nml = require('node-mod-load')('SHPS4Node-commandline');
 
-nml.addDir(__dirname + path.sep + 'interface');
-nml.addDir(__dirname + path.sep + 'src');
+nml.addDir(__dirname + path.sep + 'interface', true);
+nml.addDir(__dirname + path.sep + 'src', true);
 
 module.exports = nml.libs['coml.h'];
