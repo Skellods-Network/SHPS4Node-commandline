a495c4c7549c153b8b3d46611cc577d014b07e7f
minecrawler <me@marco-alka.de>
2017-06-23 15:38:38 +0200

    fix directory paths#

--- index-commandline.js
+++ index-commandline.js
@@ -1,8 +1,10 @@
 'use strict';
 
+const path = require('path');
+
 const nml = require('node-mod-load')('SHPS4Node-commandline');
 
-nml.addDir('interface');
-nml.addDir('src');
+nml.addDir(__dirname + path.sep + 'interface');
+nml.addDir(__dirname + path.sep + 'src');
 
 module.exports = nml.libs['coml.h'];
