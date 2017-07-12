cdbabb6f4b59df61796f8cf540f946d3a2062e23
minecrawler <me@marco-alka.de>
2017-07-12 15:31:58 +0200

    add new commands

--- src/coml.executeLine.c.js
+++ src/coml.executeLine.c.js
@@ -3,6 +3,7 @@
 const readline = require('readline');
 
 const libs = require('node-mod-load')('SHPS4Node-commandline').libs;
+const tk = require('terminal-kit').terminal;
 
 const commands = {};
 const meth = libs.meth;
@@ -18,13 +19,54 @@ meth.executeLine = function($line) {
 
     const tokens = line.split(' ');
     if (commands[tokens[0]]) {
-        commands[tokens[0]].apply(this, tokens.slice(1));
+        commands[tokens[0]].apply(this, tokens.slice(1).unshift(state));
     }
     else {
+        // todo: first propagate the command to all SHPS modules, then to all plugins
         this.writeLn(`There is no command "${tokens[0]}"`);
     }
 
     state.interface.prompt(true);
+};
+
+commands.clear = commands.cls = commands.clr = function($state) {
+    tk.clear();
+    tk.column(0);
+    $state.interface.prompt(true);
+};
+
+commands.exit = function() {
+    // todo: this should be in main
+    process.exit();
+};
+
+commands.help = function() {
+    // todo: collect command information from other modules
+    this.writeLn(`
+  Usage: command [options...]
+  
+  The SHPS CLI supports the following core commands:
+  
+  clear | cls | clr
+  ------------------------
+  Empty screen
+  
+  exit
+  ------------------------
+  Shutdown SHPS
+  
+  help
+  ------------------------
+  Display this help text
+  
+  version
+  ------------------------
+  Display version information about internal NodeJS dependencies,
+  SHPS modules and plugins
+  
+  There are additional commands to control the DB, pipelines, etc.
+  Please refer to the manual to find out more about them
+`);
 };
 
 commands.version = function() {
