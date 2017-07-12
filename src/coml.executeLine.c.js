11d327d4e899c738770f74fd88ce4d407ec2d084
minecrawler <me@marco-alka.de>
2017-07-12 14:59:31 +0200

    fix execution mechanism

--- src/coml.executeLine.c.js
+++ src/coml.executeLine.c.js
@@ -8,18 +8,32 @@ const commands = {};
 const meth = libs.meth;
 
 meth.executeLine = function($line) {
+    const line = $line.trim();
     const state = this[libs['coml-sym.h'].state];
 
-    const tokens = $line.trim().split(' ');
+    if (line === '') {
+        state.interface.prompt(true);
+        return;
+    }
+
+    const tokens = line.split(' ');
     if (commands[tokens[0]]) {
         commands[tokens[0]].apply(this, tokens.slice(1));
     }
+    else {
+        this.writeLn(`There is no command "${tokens[0]}"`);
+    }
+
+    state.interface.prompt(true);
 };
 
 commands.version = function() {
-    process.versions.forEach($v => {
-        this.writeLn(` INT: ${$v}`);
-    });
+    const v = process.versions;
+    for (let lib in v) {
+        if (v.hasOwnProperty(lib) && typeof v[lib] === 'string') {
+            this.writeLn(` INT: ${v[lib]}`);
+        }
+    }
 
     // todo: write versions of SHPS modules and plugins
 };
